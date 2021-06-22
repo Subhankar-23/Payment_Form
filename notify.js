66af628cf8ed55de035c3946c1867a43455dbfb8
Subhankar-23 <80886911+Subhankar-23@users.noreply.github.com>
2021-06-22 15:59:45 +0530

    Add files via upload

---
+++ notify.js
@@ -0,0 +1,4 @@
+alert(" Made with lots of patience.")
+var n =prompt ("Enter your name please:")
+alert("Hey "+n+"!  "+"You are onway to the payment portal. After successful transaction you will get SMS from your respective bank.")
+alert("Thankyou for using our website");
